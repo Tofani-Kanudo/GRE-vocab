3b11e7c798d554dc71857c2a40190b5fa8b9af7a
Yashesh <yashesh.mankad06@gmail.com>
2026-07-19 02:23:53 +0530

    Added base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/gre-vocab/',
   plugins: [
     react(),
     VitePWA({
@@ -15,21 +16,22 @@ export default defineConfig({
         theme_color: '#102a36',
         background_color: '#f4f9fa',
         display: 'standalone',
-        start_url: '/',
+        start_url: './',
+        scope: './',
         icons: [
           {
-            src: '/lexiloop-192.png',
+            src: 'lexiloop-192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/lexiloop-512.png',
+            src: 'lexiloop-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any',
           },
           {
-            src: '/lexiloop-512.png',
+            src: 'lexiloop-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable',
